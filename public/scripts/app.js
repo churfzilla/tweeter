b9998bd8762a844d90fc178c1701865769e15ca1
Chad Bruhaug <churfzilla@gmail.com>
2016-09-21 15:36:44 -0700

    Can now load tweets from DB with Ajax

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,53 +1,6 @@
 $(function () {
-  // Fake data taken from tweets.json
-  var data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": {
-          "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-          "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-          "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-        },
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": {
-          "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-          "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-          "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-        },
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    },
-    {
-      "user": {
-        "name": "Johann von Goethe",
-        "avatars": {
-          "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-          "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-        },
-        "handle": "@johann49"
-      },
-      "content": {
-        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-      },
-      "created_at": 1461113796368
-    }
-  ];
 
-  function createTweetElement(tweet) {
+  const createTweetElement = function createTweetElement(tweet) {
     var $tweet = $('<article>').addClass('tweet');
     var $tweetHeader = $('<header>').appendTo($tweet);
 
@@ -85,7 +38,21 @@ $(function () {
     return $tweet;
   }
 
-  function renderTweets(tweets) {
+  $('#new-tweet').on('submit', function (event) {
+    event.preventDefault();
+    const theForm = $(this);
+    $.ajax({
+      url: theForm.attr('action'),
+      method: theForm.attr('method'),
+      data: theForm.serialize(),
+      dataType: 'json',
+      success: function () {
+        createTweetElement();
+      }
+    });
+  });
+
+  const renderTweets = function renderTweets(tweets) {
     tweets.sort(function (a, b) {
       return b.created_at - a.created_at;
     });
@@ -94,6 +61,21 @@ $(function () {
     });
   }
 
-  renderTweets(data);
-
+  const loadTweets = () => {
+  // Reads the tweets from the database and renders to the website
+    $.get('/tweets')
+      .then(renderTweets)
+      .then(() => {
+        $('#new-tweet').val('');
+      })
+  }
+    const validateForm = () => {
+      const tweetLength = $('.new-tweet form textarea').val().length;
+      if (tweetLength > 140) {
+        return false;
+      } else {
+        return true;
+      }
+    }
+  loadTweets();
 });

3560eb4443a7e97a5137d06fc4846a703002e375
Chad Bruhaug <churfzilla@gmail.com>
2016-09-23 13:36:08 -0700

    Finished and reviewed (without cookie inclusion)

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,6 +5,7 @@ const WARNING_CLASS = "redText";
 
 let $textArea = $( ".new-tweet form textarea" );
 let $counter = $($textArea.siblings(".counter")[0]);
+
 let updateCounter = function() {
   let tweetLength = this.value.length;
   $counter.text(TWEETLENGTHMAX - tweetLength);
@@ -27,9 +28,13 @@ $textArea.on("input", updateCounter);
   }
 
 // Creates the Tweet element in the DOM -----------------------------
-  const createTweetElement = function createTweetElement(tweet) {
+  const createTweetElement = function(tweet) {
     var $tweet = $('<article>').addClass('tweet');
     var $tweetHeader = $('<header>').appendTo($tweet);
+    var $tweetFooter = $('<footer>').text(moment(tweet.created_at).fromNow())
+      .appendTo($tweet);
+    var $tweetFooterIcons = $('<div>')
+      .appendTo($tweetFooter);
 
     $('<img>').attr('src', tweet.user.avatars.small)
       .attr('alt', tweet.user.name + '\'s profile image')
@@ -43,12 +48,6 @@ $textArea.on("input", updateCounter);
 
     $('<section>').text(tweet.content.text)
       .appendTo($tweet);
-
-    var $tweetFooter = $('<footer>').text(moment(tweet.created_at).fromNow())
-      .appendTo($tweet);
-
-    var $tweetFooterIcons = $('<div>')
-      .appendTo($tweetFooter);
 
     $('<i>').addClass('fa fa-font-awesome')
       .attr('aria-hidden', 'true')
@@ -73,20 +72,24 @@ $textArea.on("input", updateCounter);
 
   $('section.new-tweet form').on('submit', function (e) {
     e.preventDefault();
-    var form = $(this);
-    validateTweet(form);
+    validateTweet($(this));
   });
 
+  /*
+    validates tweet
+    - text not null
+    - less than x chars
+
+    returns false when not valid; true otherwise
+  */
   function validateTweet(form) {
     var text = form.children('textarea[name=text]').val();
     var err = form.parent().children('div.error-container');
 
     if (text === '') {
-      alert('Nothing to say? Then kick rocks!');
-      return false;
+      return alert('Nothing to say? Then kick rocks!');
     } else if (text.length > TWEETLENGTHMAX) {
-      alert('Gear down big-rig, you have written too much');
-      return false;
+      return alert('Gear down big-rig, you have written too much');
     }
 
     $.ajax({
@@ -99,31 +102,34 @@ $textArea.on("input", updateCounter);
             $('#tweets-container').prepend(createTweetElement(tweets[0]));
             err.text('');
             form.children('textarea[name=text]').val('');
-            form.children('span.counter').text(TWEETLENGTHMAX).css('color', 'inherit');
+            form.children('span.counter').text(TWEETLENGTHMAX);
           });
         }
       });
     return true;
   }
 
-    $( "#nav-bar .compose" ).click(function() {
-      $( "section.new-tweet" ).slideToggle( "slow", function() {
-        $.fn.setCursorPosition = function(pos) {
-          this.each(function(index, elem) {
-            if (elem.setSelectionRange) {
-              elem.setSelectionRange(pos, pos);
-            } else if (elem.createTextRange) {
-                var range = elem.createTextRange();
-                range.collapse(true);
-                range.moveEnd('character', pos);
-                range.moveStart('character', pos);
-                range.select();
-            }
-          });
-          return this;
-        };
-        $('.new-tweet form textarea').setCursorPosition(0);
-      });
+  // Sets the cursor into the text area - called when animation is finished
+  $.fn.setCursorPosition = function(pos) {
+    this.each(function(index, elem) {
+      if (elem.setSelectionRange) {
+        elem.setSelectionRange(pos, pos);
+      } else if (elem.createTextRange) {
+          var range = elem.createTextRange();
+          range.collapse(true);
+          range.moveEnd('character', pos);
+          range.moveStart('character', pos);
+          range.select();
+      }
     });
+    return this;
+  };
+
+ // creates a toggle animation for the composing of a new tweet
+  $( "#nav-bar .compose" ).click(function() {
+    $( "section.new-tweet").slideToggle("slow", function() {
+      $('.new-tweet form textarea').setCursorPosition(0);
+    });
+  });
   loadTweets(renderTweets);
 });
